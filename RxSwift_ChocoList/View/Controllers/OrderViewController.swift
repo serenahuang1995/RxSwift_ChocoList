8f1e7aebc842bb271de1566a642906e37147c77c
黃瀞萱 <serena@happyde-mbp.udnprod.local>
2021-09-27 11:54:16 +0800

    fix constraint

--- RxSwift_ChocoList/View/Controllers/OrderViewController.swift
+++ RxSwift_ChocoList/View/Controllers/OrderViewController.swift
@@ -26,7 +26,7 @@ extension OrderViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configure(title: .order)
-//        configureSubViews()
+        configureSubViews()
         setupCardIcon()
         setupInfoFromCart()
     }
@@ -47,31 +47,37 @@ extension OrderViewController {
         orderStackView.spacing = 0
         orderStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        let screen = UIScreen.main.bounds
-        let scrollView = UIScrollView(frame: screen)
-        scrollView.isScrollEnabled = true
-        scrollView.contentSize = CGSize(width: screen.width, height: image.frame.height + orderStackView.frame.height + 40)
-        [image, orderStackView].forEach {
-            scrollView.addSubview($0)
-            scrollView.translatesAutoresizingMaskIntoConstraints = false
-        }
+        image.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(image)
         
-        [scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//        let screen = UIScreen.main.bounds
+//        let scrollView = UIScrollView(frame: screen)
+//        scrollView.isScrollEnabled = true
+//        scrollView.contentSize = CGSize(width: screen.width, height: image.frame.height + orderStackView.frame.height + 40)
+//        [image, orderStackView].forEach {
+//            scrollView.addSubview($0)
+//            scrollView.translatesAutoresizingMaskIntoConstraints = false
+//        }
+        
+        [
+//         scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+//         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//         scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
          
          image.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
-         image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+         image.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
          image.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
          image.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
          image.widthAnchor.constraint(equalTo: image.heightAnchor, multiplier: 160/133),
-         image.heightAnchor.constraint(equalTo: image.widthAnchor, multiplier: 133/160),
             
          orderStackView.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 20),
          orderStackView.leadingAnchor.constraint(equalTo: image.leadingAnchor, constant: 10),
-         orderStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20)
+         orderStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20),
          
+         cardImage.heightAnchor.constraint(equalToConstant: 30),
+         cardImage.widthAnchor.constraint(equalToConstant: 48)
+
         ].forEach{ $0.isActive = true }
     }
 }
